50f13778d99ab1e09b58be2d4abf16c69fb9f9e6
katepisova <katepisova97@gmail.com>
2018-07-04 23:43:02 +0300

    Adding plotting to code.

--- middle_point.m
+++ middle_point.m
@@ -1,31 +1,40 @@
 function middle_point
-% [a, b] - заданный промежуток
+arr = [];
 a = -3.2; 
 b = 0.9;
-eps = 10e-3; % заданная точность
+eps = 10e-3; 
 answ = 0;
-while b - a > eps % пока не достигнем заданной точности
-    middle = (a + b) / 2; % середина отрезка
-    D = df(middle); % значение производной в середине отрезка
+middle = (a + b) / 2;
+arr = [arr middle];
+while b - a > eps 
+    middle = (a + b) / 2; 
+    D = df(middle);
     if D > 0 
         b = middle;
     elseif D < 0 
-        a = middle
+        a = middle;
     else
         answ = middle;
     end
+    
+    arr =[arr middle];
 end
 f_x_min = f(middle);
-disp('Точка минимума =');
 disp(middle);
-disp('Значение функции в точке минимума =');
 disp(f_x_min);
+x = -3.2 : 0.01 : 0.9;
+f_x = f(x);
+plot(x,f_x,'b'); 
+hold on;
+f_x_arr = f(arr);
+plot(arr,f_x_arr,'ro'); 
+
 end
 
-function f = f(x) % исходная функция
-    f = (x + 1)*(x + 1)*(x + 1)*(x + 1) - 2 * x * x;
+function f = f(x) 
+    f = (x + 1).*(x + 1).*(x + 1).*(x + 1) - 2 .* x .* x;
 end
 
-function df = df(x) % производная функции
-    df = 4 * (x + 1)*(x + 1)*(x + 1) - 4 * x;
+function df = df(x) 
+    df = 4 .* (x + 1).*(x + 1).*(x + 1) - 4 .* x;
 end
