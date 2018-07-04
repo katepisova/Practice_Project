a65cbba51ca7825bd2bbd18cb63495f2ebdb374e
katepisova <katepisova97@gmail.com>
2018-07-04 23:33:06 +0300

    Adding some comments to code.

--- middle_point.m
+++ middle_point.m
@@ -1,11 +1,12 @@
 function middle_point
+% [a, b] - заданный промежуток
 a = -3.2; 
 b = 0.9;
-eps = 10e-3; 
+eps = 10e-3; % заданная точность
 answ = 0;
-while b - a > eps 
-    middle = (a + b) / 2; 
-    D = df(middle); 
+while b - a > eps % пока не достигнем заданной точности
+    middle = (a + b) / 2; % середина отрезка
+    D = df(middle); % значение производной в середине отрезка
     if D > 0 
         b = middle;
     elseif D < 0 
@@ -21,10 +22,10 @@ disp('Значение функции в точке минимума =');
 disp(f_x_min);
 end
 
-function f = f(x) 
+function f = f(x) % исходная функция
     f = (x + 1)*(x + 1)*(x + 1)*(x + 1) - 2 * x * x;
 end
 
-function df = df(x) 
+function df = df(x) % производная функции
     df = 4 * (x + 1)*(x + 1)*(x + 1) - 4 * x;
 end
